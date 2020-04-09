76a8419caff2d2ef1e8b7fe6631f2eaab5ed18d4
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-09 11:50:31 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -25,8 +25,8 @@ class Waiter
   def best_tipper
     best_tipped_meal = meals.max do |meal_a,meal_b|
       meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal
   end
-  best_tipped_meal
-end
 
 end
