5a30890d85edc9d3f6b79cccf4da815aae596d5f
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-09 11:31:13 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,10 @@
 class Waiter
+  attr_accessor :name, :years_experience
+  @@all = []
+
+  def initialize(name,years_experience)
+    @name = name
+    @years_experience = years_experience
+    
 
 end
